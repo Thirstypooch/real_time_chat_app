72495d3840303e71bfd4c0c61c02bf55ab671f36
Thirstypooch <juancocchi6@gmail.com>
2025-07-21 17:13:52 -0500

    better client config to connect reverb and fly proxy

--- src/services/echo.ts
+++ src/services/echo.ts
@@ -1,41 +1,33 @@
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
-import apiClient from './api';
-
-interface ChannelAuthorizationData {
-    auth: string;
-}
 
 const pusherClient = new Pusher(window.runtimeConfig.VITE_REVERB_APP_KEY, {
     wsHost: window.runtimeConfig.VITE_REVERB_HOST,
-    wsPort: Number(window.runtimeConfig.VITE_REVERB_PORT || 8080),
-    wssPort: Number(window.runtimeConfig.VITE_REVERB_PORT || 8080),
+    wsPort: Number(window.runtimeConfig.VITE_REVERB_PORT),
+    wssPort: Number(window.runtimeConfig.VITE_REVERB_PORT),
     forceTLS: window.runtimeConfig.VITE_REVERB_SCHEME === 'https',
     enabledTransports: ['ws', 'wss'],
     cluster: 'mt1',
-
-    authorizer: (channel: { name: string }) => ({
-        authorize: (
-            socketId: string,
-            callback: (error: Error | null, data: ChannelAuthorizationData | null) => void
-        ) => {
-            apiClient.post('/broadcasting/auth', {
-                socket_id: socketId,
-                channel_name: channel.name
-            })
-                .then(response => {
-                    callback(null, response.data);
-                })
-                .catch(error => {
-                    callback(error, null);
-                });
-        }
-    }),
+    authEndpoint: `${window.runtimeConfig.VITE_API_URL}/broadcasting/auth`,
+    auth: {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('api_token')}`,
+            Accept: 'application/json',
+        },
+    },
 });
 
 const echo = new Echo({
     broadcaster: 'pusher',
     client: pusherClient,
+});
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const pusherConnector = echo.connector as any;
+
+pusherConnector.pusher.connection.bind('connected', () => {
+    if (pusherConnector.options.auth?.headers) {
+        pusherConnector.options.auth.headers.Authorization = `Bearer ${localStorage.getItem('api_token')}`;
+    }
 });
 
 export default echo;

30701480d6e9e7828aad5817be3d1a721e7faef2
Thirstypooch <juancocchi6@gmail.com>
2025-07-21 13:11:18 -0500

    correct the sed command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 set -e
 
-# Find the built index.html file
-# Note: Added a more robust find command
 INDEX_FILE=$(find /usr/share/caddy -name 'index*.html' | head -n 1)
 
-# Check if the file was found
 if [ -z "$INDEX_FILE" ]; then
   echo "Error: index.html not found in /usr/share/caddy"
   exit 1
 fi
 
-# Replace placeholders with environment variables using the corrected sed syntax
-sed -i '' "s|__VITE_API_URL__|${VITE_API_URL}|g" $INDEX_FILE
-sed -i '' "s|__VITE_REVERB_APP_KEY__|${VITE_REVERB_APP_KEY}|g" $INDEX_FILE
-sed -i '' "s|__VITE_REVERB_HOST__|${VITE_REVERB_HOST}|g" $INDEX_FILE
-sed -i '' "s|__VITE_REVERB_PORT__|${VITE_REVERB_PORT}|g" $INDEX_FILE
-sed -i '' "s|__VITE_REVERB_SCHEME__|${VITE_REVERB_SCHEME}|g" $INDEX_FILE
+# Use -i.bak for Alpine sed compatibility and chain sed commands.
+sed -i.bak \
+    -e "s|__VITE_API_URL__|${VITE_API_URL}|g" \
+    -e "s|__VITE_REVERB_APP_KEY__|${VITE_REVERB_APP_KEY}|g" \
+    -e "s|__VITE_REVERB_HOST__|${VITE_REVERB_HOST}|g" \
+    -e "s|__VITE_REVERB_PORT__|${VITE_REVERB_PORT}|g" \
+    -e "s|__VITE_REVERB_SCHEME__|${VITE_REVERB_SCHEME}|g" \
+    "$INDEX_FILE"
+
+# Remove the backup file created by sed
+rm "${INDEX_FILE}.bak"
 
 echo "Runtime config injected. Starting Caddy server."
 
-# Start the Caddy server
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile

19e48eb9e0c89575d953622f2f4e691bd8b9fb79
Thirstypooch <juancocchi6@gmail.com>
2025-07-21 23:36:12 -0500

    clear cache

--- chat-api/.fly/scripts/caches.sh
+++ chat-api/.fly/scripts/caches.sh
@@ -2,5 +2,5 @@
 
 # Use the correct path for the PHP executable
 /usr/local/bin/php /var/www/html/artisan config:cache --no-ansi -q
-/usr/local/bin/php /var/www/html/artisan route:cache --no-ansi -q
+# /usr/local/bin/php /var/www/html/artisan route:cache --no-ansi -q
 /usr/local/bin/php /var/www/html/artisan view:cache --no-ansi -q
